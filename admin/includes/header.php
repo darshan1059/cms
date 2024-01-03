1b96be3846ddc8208c8badf4b0ff483115daf8c9
darshan1059 <chandradarshanagrawal1059@gmail.com>
2024-01-03 20:05:48 +0530

    Login and Logout functionality implemented

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -2,7 +2,16 @@
 <?php include "functions.php"; ?>
 
 <?php ob_start(); ?>
+<?php session_start(); ?>
 
+
+<?php
+
+if (!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+}
+
+?>
 
 
 
--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -19,12 +19,12 @@
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
-                    <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                    <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
                 </li>
 
                 <li class="divider"></li>
                 <li>
-                    <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                    <a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                 </li>
             </ul>
         </li>
@@ -69,7 +69,7 @@
             </li>
 
             <li>
-                <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
+                <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
             </li>
 
         </ul>
